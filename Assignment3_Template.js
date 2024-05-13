03c6649ebef7d84793687feaeb66412e02b81b5f
Nicholas-Garbutt8 <126737928+Nicholas-Garbutt8@users.noreply.github.com>
2024-05-13 14:01:54 +1000

    Created the heading text and added blank image

--- Assignment3_Template.js
+++ Assignment3_Template.js
@@ -1,18 +1,36 @@
 import React from "react";
-import {SafeAreaView, StyleSheet} from 'react-native';
-import {Picker} from '@react-native-picker/picker'; 
+import { SafeAreaView, StyleSheet, Text, Image } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+
 
 export default function App() {
   return (
-  <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.heading1}>POGS</Text>
+      <Image source={require('./placeholder.png')} style={styles.image} />
 
 
-  </SafeAreaView>
-  );}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#ffffcc'
-    },
-  });
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center'
+  },
+  heading1: {
+    textAlign: "center",
+    fontSize: 24, 
+    fontWeight: 'bold' 
+  },
+  image: {
+    marginTop: 25,
+    width: 250, 
+    height: 250, 
+  },
+
+
+});
